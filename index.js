a29e32270eedb5d9fd0051665f1d5dfefbba9345
david <david.beritashvili2003@gmail.com>
2023-03-29 20:54:16 +0400

    iitial

---
+++ index.js
@@ -0,0 +1,73 @@
+// //დავალება 1
+//  let userIsLoggedIn = true
+//  let videoIsCommentable = true
+//  console.log (userCanComment = userIsLoggedIn && videoIsCommentable)
+
+// დავალება 2
+//  let userIsLoggedIn = true;
+//  let userIsBlocked = true;
+//  console.log((userCanPost = !(userIsBlocked) && userIsLoggedIn));
+
+// დავალება 3
+//let balance = 600
+//let userCartTotal = 500
+//console.log(paymentComplete = balance >= userCartTotal)
+
+// დავალება 4
+//let userIsAdmin = true
+//let userIsModerator = false
+//let userIsCommentAuthor =  false
+//console.log(userCanDelete = userIsAdmin || userIsModerator || userIsCommentAuthor)
+
+//დავალება 5
+//let presidentKey = true
+//let primeMinisterKey = true
+//let generalOfArmiesKey = true
+//let masterKey = false
+//console.log(bombWillLaunch = presidentKey&& primeMinisterKey && generalOfArmiesKey || (masterKey))
+
+//დავალება 6
+//const userPinCode = 12345;
+//const userCurrentPinCode = 12345;
+//const newPinCode = 54321;
+//const reTypePinCode = 54321;
+
+//const pinChangedComplete = userPinCode == userCurrentPinCode && newPinCode == reTypePinCode;
+//console.log(pinChangedComplete);
+
+
+//დავალება 7
+//let health = 100
+//let lives = 3
+
+//health= 0
+//lives = 0
+
+//const gameOVer = health < 1 && lives < 1
+//console.log(gameOVer)
+
+//დავალება 8
+
+// let capsuleCount = 1
+// let capsulePrice = 5
+// let userBalance = 5
+
+// const creditCardStatus = capsuleCount >0 && capsulePrice <= userBalance
+
+// const paymentComplete = capsuleCount > 0 && creditCardStatus
+// console.log (paymentComplete)
+
+//დავალება 9
+
+// let avliableRooms = 10
+// let roomPrice = 30
+
+// let disCount = roomPrice / 100 * 50 // % :))
+// let userBalance = 25
+// let userHasDiscount = true
+// let userRoomCount = 1
+
+// const paymentComplate = userRoomCount <= avliableRooms && userBalance >= (roomPrice * userRoomCount) - (userHasDiscount && disCount)
+// console.log(paymentComplate)
+
+
