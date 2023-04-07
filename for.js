4c637c90c829c78f6da40bba2804f56c2882c8c0
david <david.beritashvili2003@gmail.com>
2023-04-07 20:28:18 +0400

    + interations homework

--- for.js
+++ for.js
@@ -35,16 +35,50 @@
 // console.log(sum);
 
 // დავალება 5
-odd = 0;
-even = 0;
-for (let i = 500; i < 800; i++) {
-  if (i % 2 == 1) {
-    odd += i;
-  }
-  for (let index = 1000; index < 2000; index++) {
-    if (i % 2 == 0) {
-      even += index;
-    }
-  }
+// odd = 0;
+// even = 0;
+// for (let i = 500; i < 800; i++) {
+//   if (i % 2 == 1) {
+//     odd += i;
+//   }
+//   for (let index = 1000; index < 2000; index++) {
+//     if (i % 2 == 0) {
+//       even += index;
+//     }
+//   }
+// }
+// console.log(odd + even);
+
+//დავალება 6
+// let num = 5
+// let fact = 1
+// for( let i = num ; i>=1; i--){
+//   fact = fact * i
+// }
+// console.log(fact);
+
+//  დავალება 7
+// for (i = 0; i <= 300; i++) {
+//   if (300 % i == 0)
+//    console.log(i);
+// }
+
+// დავალება 8
+// let sum = 0;
+
+// for (i = 0; i <= 300; i++) {
+//   if (300 % i == 0)
+//   sum += i
+
+// }
+// console.log(sum);
+
+// დავალება 9
+
+let x = 8;
+
+if (x == 2 || (x % 2 == 1 && x % x == 0)) {
+  console.log("მარტივია");
+} else {
+  console.log(" რტულიაააა");
 }
-console.log(odd + even);
